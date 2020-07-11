b480791e953a5712613445ad75126f978c5cfc60
DESKTOP-LNI90NK\JS <jinsol0330@naver.com>
2020-07-11 22:05:23 +0900

    이벤트와 이벤트 핸들러
    윈도우 크기가 변경되면 함수 호출

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const title = document.querySelector("#title");
 //id=#, class=.
-title.innerHTML = "Hi From JS";
-title.style.color = "red";
-console.dir(title);
+// title.innerHTML = "Hi From JS";
+// title.style.color = "red";
+// document.title="I owe you now";
+
+function handleResize(event) {
+
+    console.log("I have been resized");
+}
+
+window.addEventListener("resize", handleResize);
+//함수를 바로 즉시 호출하지 않음
+//내가 필요한 시점에 호출
+//만약 handleResize() 라고 한다면 즉시 호출
+
