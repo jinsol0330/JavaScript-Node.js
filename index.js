6c6257a35336cee56c85617cf82a53949f9044af
DESKTOP-LNI90NK\JS <jinsol0330@naver.com>
2020-07-11 21:50:05 +0900

    querySelector
    노드의 첫번째 자식을 반환함

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-const title = document.getElementById(title);
+const title = document.querySelector("#title");
+//id=#, class=.
 title.innerHTML = "Hi From JS";
+title.style.color = "red";
+console.dir(title);
