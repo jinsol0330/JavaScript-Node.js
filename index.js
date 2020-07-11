9cb5c197faa94ed1d209957f969a4b23029a6972
DESKTOP-LNI90NK\JS <jinsol0330@naver.com>
2020-07-11 22:07:59 +0900

    이벤트와 이벤트 핸들러2
    클릭하면 색이 바뀜

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const title = document.querySelector("#title");
-//id=#, class=.
-// title.innerHTML = "Hi From JS";
-// title.style.color = "red";
-// document.title="I owe you now";
 
-function handleResize(event) {
-
-    console.log("I have been resized");
+function handleClick() {
+    title.style.color = "blue";
 }
 
-window.addEventListener("resize", handleResize);
-//함수를 바로 즉시 호출하지 않음
-//내가 필요한 시점에 호출
-//만약 handleResize() 라고 한다면 즉시 호출
-
+title.addEventListener("click", handleResize);
