40e96b560929373235b502b1851531506a5b3140
DESKTOP-LNI90NK\JS <jinsol0330@naver.com>
2020-07-11 22:23:46 +0900

    이벤트와 이벤트 핸들러3
    if-else문 사용

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const title = document.querySelector("#title");
 
+const BASE_COLOR = "rgb(52,73,94)";
+const OTHER_COLOR = "7f8c8d";
+
 function handleClick() {
-    title.style.color = "blue";
+    const currentColor = title.style.color;
+    if (currentColor == BASE_COLOR) {
+        title.style.color = OTHER_COLOR; 
+    }
+    else {
+        title.style.color = BASE_COLOR;
+    }
 }
 
-title.addEventListener("click", handleResize);
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleResize);
+    //클릭했을 때
+}
+init();
+
+function handleOffline() {
+    console.log("lalalala");
+}
+
+function handleOnline() {
+    cosole.log("Welcome back");
+}
+
+window.addEventListener("offline", handleOffline);
+//와이파이가 꺼졌을 때
+window.addEventListener("online", handleOnline);
+//와이파이가 켜졌을 때
+
